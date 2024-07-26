5dc0f8cf87f27e1fcc8d7d68c0565fee1bae50b3
Peter Watson <59647390+PJ-Watson@users.noreply.github.com>
2024-07-26 12:24:15 +0200

    Add wrapper around AsinhStretch to allow for raw data units.

--- src/astrocolour/main.py
+++ src/astrocolour/main.py
@@ -7,6 +7,8 @@ import astropy.visualization as visualization
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing as npt
+from astropy.stats import sigma_clipped_stats
+from matplotlib.colors import hsv_to_rgb, rgb_to_hsv
 
 from . import utils
 
@@ -23,8 +25,6 @@ class ColourImage:
         _description_, by default None.
     crop : npt.ArrayLike | None, optional
         _description_, by default None.
-    transformation : _type_, optional
-        _description_, by default None.
     transformation_kwargs : _type_, optional
         _description_, by default None.
     colours : _type_, optional
@@ -38,7 +38,7 @@ class ColourImage:
         images: npt.ArrayLike,
         img_extns: npt.ArrayLike | None = None,
         crop: npt.ArrayLike | None = None,
-        transformation=None,
+        # transformation=None,
         transformation_kwargs=None,
         colours=None,
         colours_kwargs=None,
@@ -47,7 +47,7 @@ class ColourImage:
 
         if transformation_kwargs is None:
             transformation_kwargs = {}
-        self.apply_transformation(transformation, **transformation_kwargs)
+        self.apply_transformation(**transformation_kwargs)
 
         if colours_kwargs is None:
             colours_kwargs = {}
@@ -55,7 +55,26 @@ class ColourImage:
 
         self.apply_colours()
         self.combine_to_rgb()
-        self.plot()
+        # self.plot()
+
+    def __array__(self):
+        return self.rgb
+
+    def __getitem__(self, key):
+        """
+        Slice the RGB image.
+        """
+        if (
+            isinstance(key, tuple)
+            and len(key) == 2
+            and all(
+                isinstance(key[i], slice) and (key[i].start != key[i].stop)
+                for i in (0, 1)
+            )
+        ):
+            return self.rgb[key]
+        else:
+            raise TypeError(f"{key!r} is not a valid 2D slice object")
 
     def _validate_images_input(self, images, img_extns):
 
@@ -65,6 +84,12 @@ class ColourImage:
             self._validate_imgs_array(images)
 
     def _validate_imgs_array(self, images):
+        """
+        Validates the image input, if given as an array-like.
+
+        Checks that the N input images all have the same shape (i x j), and creates
+        an N x i x j array to store them.
+        """
 
         self.images = []
         for img in images:
@@ -75,32 +100,47 @@ class ColourImage:
                 )
             if img_view.ndim > 2:
                 self.images.append(
-                    np.nanmedian(img_view, axis=0).reshape(
+                    sigma_clipped_stats(img_view, axis=0)[1].reshape(
                         img_view.shape[1], img_view.shape[2]
                     )
                 )
 
         self.image_shape = self.images[0].shape
         self.N = len(self.images)
-        self.images = np.asarray(self.images)
+
         assert all(
             [i.shape == self.image_shape for i in self.images]
         ), "The dimensions of the input images do not match."
 
+        self.images = np.asarray(self.images)
+
     def apply_transformation(
         self,
+        stretch: visualization.BaseStretch | None = visualization.AsinhStretch(a=1e-2),
+        interval: visualization.BaseInterval | None = visualization.PercentileInterval(
+            99.9
+        ),
         transformation: visualization.BaseTransform | None = None,
         per_image: bool = False,
     ):
         """
-        _summary_.
+        Apply a transformation (interval and stretch) to the images.
 
         Parameters
         ----------
+
+        stretch : visualization.BaseStretch | None, optional
+            The stretch to be applied to the image. This defaults to an Asinh stretch, with
+            the logarithmic transition occurring at 1% of the normalised maximum intensity.
+        interval : visualization.BaseInterval | None, optional
+            The interval within which the image will be normalised. By default, this is
+            99% of the full intensity range.
         transformation : visualization.BaseTransform | None, optional
-            _description_, by default None.
+            Any transformation to be applied to the image. This overrides all other
+            options (`stretch` and `interval`) if supplied, by default None.
         per_image : bool, optional
-            _description_, by default False.
+            Apply the transformation to each input image individually. This may result
+            in inconsistent scaling if using percentile intervals, and is False by default.
         """
 
         if transformation is not None:
@@ -108,22 +148,28 @@ class ColourImage:
                 transformation, visualization.BaseTransform
             ), "`transformation` must be an instance of `astropy.visualization.BaseTransform`."
             self.transformation = transformation
+        elif stretch is not None and interval is not None:
+            self.transformation = stretch + interval
+        elif stretch is not None and interval is None:
+            self.transformation = stretch
+        elif stretch is None and interval is not None:
+            self.transformation = interval
         else:
-            self.transformation = visualization.AsinhStretch(
-                a=0.01
-            ) + visualization.PercentileInterval(99)
+            # Should this be a warning instead?
+            raise ValueError("No valid transformation supplied.")
 
         if per_image:
             for i, frame in enumerate(self.images):
-                self.images[i] = self.transformation(frame)
+                self.images[i] = self.transformation(frame, clip=False)
         else:
-            self.images = self.transformation(self.images)
+            self.images = self.transformation(self.images, clip=False)
 
     def generate_colours(
         self,
         colours: npt.ArrayLike | None = None,
         col_limits: npt.ArrayLike | None = None,
         hsv: bool = False,
+        **kwargs,
     ):
         """
         Generate a colour for each input image.
@@ -137,6 +183,8 @@ class ColourImage:
         hsv : bool, optional
             Interpret input colour tuples (a,b,c) as (H,S,V) rather than the
             default (R,G,B). By default False.
+        **kwargs : dict, optional
+            Other arguments to pass to `utils.interpolate_colours`.
         """
 
         if colours is not None:
@@ -149,9 +197,11 @@ class ColourImage:
             assert (
                 len(col_limits) == 2
             ), "`col_limits` must contain exactly two colours."
-            self.colours = utils.interpolate_colours(col_limits, self.N, hsv)
+            self.colours = utils.interpolate_colours(col_limits, self.N, hsv, **kwargs)
         else:
-            self.colours = utils.interpolate_colours([(0, 0, 1), (1, 0, 0)], self.N)
+            self.colours = utils.interpolate_colours(
+                [(0, 0, 1), (1, 0, 0)], self.N, **kwargs
+            )
 
     def apply_colours(self):
         """
@@ -161,20 +211,164 @@ class ColourImage:
         self.rgb_frames = []
         for i, (frame, col) in enumerate(zip(self.images, self.colours)):
             cmap = utils.gen_cmap_from_rgba(col)
-            print(col)
+            # print(col)
 
             self.rgb_frames.append(cmap(frame))
         self.rgb_frames = np.asarray(self.rgb_frames)
+        self.rgb_frames_scaling = np.array([1.0, 1.0, 1.0])
 
-    def combine_to_rgb(self):
+    def combine_to_rgb(self, method="sum"):
         """
         _summary_.
-        """
-        self.rgb = np.nanmax(self.rgb_frames, axis=0)
 
-    def plot(self):
+        Parameters
+        ----------
+        method : str, optional
+            _description_, by default "sum".
+        """
+        match method:
+            case "max":
+                self.rgb = np.nanmax(self.rgb_frames, axis=0)
+            case "sum":
+                self.rgb = (
+                    np.nansum(self.rgb_frames, axis=0)
+                    / np.nansum(self.colours, axis=0)[np.newaxis, np.newaxis, :]
+                )
+
+        self.rgb_scaling = np.array([1.0, 1.0, 1.0])
+
+    def plot(self, ax=None, individual=False):
         """
         _summary_.
+
+        Parameters
+        ----------
+        ax : _type_, optional
+            _description_, by default None.
+        individual : bool, optional
+            _description_, by default False.
+
+        Returns
+        -------
+        fig, ax
+            _description_.
         """
-        plt.imshow(self.rgb, origin="lower")
-        plt.show()
+        if individual:
+            columns = np.ceil(np.sqrt(self.N)).astype(int)
+            rows = np.ceil(self.N / columns).astype(int)
+
+            fig, axs_2d = plt.subplots(rows, columns, sharex=True, sharey=True)
+            axs = axs_2d.flatten()
+
+            for i, f in enumerate(self.rgb_frames):
+                axs[i].imshow(f, origin="lower")
+            return fig, axs_2d
+        else:
+            if ax is not None:
+                fig = ax.get_figure()
+            else:
+                fig, ax = plt.subplots()
+            ax.imshow(self.rgb, origin="lower")
+            return fig, ax
+        # plt.show()
+        # return
+
+    def set_white_balance(self, centre, radius, method="sigma_clip"):
+        """
+        _summary_.
+
+        Parameters
+        ----------
+        centre : _type_
+            _description_.
+        radius : _type_
+            _description_.
+        method : str, optional
+            _description_, by default "sigma_clip".
+        """
+
+        rr, cc = utils.circle_coords(centre, radius, self.image_shape)
+        # print(self.rgb.shape)
+        # print(self.rgb[rr, cc, :3].shape)
+        match method:
+            case "median":
+                avg_fn = np.nanmedian
+            case "mean":
+                avg_fn = np.nanmean
+            case "sigma_clip":
+                avg_fn = sigma_clipped_stats
+
+        self.rgb[:, :, :3] /= self.rgb_scaling
+
+        rgb_current = avg_fn(self.rgb[rr, cc, :3], axis=0)
+        if method == "sigma_clip":
+            rgb_current = rgb_current[0]
+        # rgb_current = sigma_clip(self.rgb[rr,cc,:3],axis=0)
+        # print (rgb_current)
+        rgb_current[rgb_current == 0] = np.nanmedian(rgb_current[rgb_current != 0])
+        # print (rgb_current)
+        # print(np.nanmean(rgb_current) / rgb_current)
+        # print (np.nanmin(self.rgb[rr,cc,:3]))
+        self.rgb_scaling = np.nanmean(rgb_current) / rgb_current
+        self.rgb[:, :, :3] *= self.rgb_scaling
+        # for i, scale in enumerate(np.nanmean(rgb_current) / rgb_current):
+        #     # for i, scale in enumerate(rgb_current/np.nanmean(rgb_current)):
+        #     self.rgb[:, :, i] *= scale
+
+    def adjust_contrast(self, cutoff=0.5, gain=10, inv=False):
+        """
+        _summary_.
+
+        Parameters
+        ----------
+        cutoff : float, optional
+            _description_, by default 0.5.
+        gain : int, optional
+            _description_, by default 10.
+        inv : bool, optional
+            _description_, by default False.
+        """
+
+        # #skimage.exposure.adjust_sigmoid
+        # if cutoff is None:
+        #     cutoff = np.nanmedian(self.rgb[:,:,:-1])
+
+        # scale = float(dtype_limits(image, True)[1] - dtype_limits(image, True)[0])
+        scale = 1.0
+
+        if inv:
+            self.rgb[:, :, :-1] = (
+                1 - 1 / (1 + np.exp(gain * (cutoff - self.rgb[:, :, :-1] / scale)))
+            ) * scale
+
+        else:
+            self.rgb[:, :, :-1] = (
+                1 / (1 + np.exp(gain * (cutoff - self.rgb[:, :, :-1] / scale)))
+            ) * scale
+
+    def adjust_saturation(self, factor=1.25):
+        """
+        _summary_.
+
+        Parameters
+        ----------
+        factor : float, optional
+            _description_, by default 1.25.
+        """
+
+        hsv_copy = rgb_to_hsv(self.rgb[:, :, :-1])
+        hsv_copy[:, :, 1] *= factor
+        self.rgb[:, :, :-1] = hsv_to_rgb(hsv_copy)
+
+        # # #skimage.exposure.adjust_sigmoid
+        # # if cutoff is None:
+        # #     cutoff = np.nanmedian(self.rgb[:,:,:-1])
+
+        # # scale = float(dtype_limits(image, True)[1] - dtype_limits(image, True)[0])
+        # scale = 1.
+
+        # if inv:
+        #     self.rgb[:,:,:-1] = (1 - 1 / (1 + np.exp(gain * (cutoff - self.rgb[:,:,:-1] / scale)))) * scale
+
+        # else:
+        #     self.rgb[:,:,:-1] = (1 / (1 + np.exp(gain * (cutoff - self.rgb[:,:,:-1] / scale)))) * scale
--- src/astrocolour/utils.py
+++ src/astrocolour/utils.py
@@ -1,6 +1,15 @@
 """Various utility functions."""
 
 import numpy as np
+import numpy.typing as npt
+from astropy.visualization import (
+    AsinhStretch,
+    BaseStretch,
+    BaseTransform,
+    LinearStretch,
+    LogStretch,
+    SqrtStretch,
+)
 from matplotlib.colors import ListedColormap, hsv_to_rgb, rgb_to_hsv, to_rgba
 
 
@@ -88,18 +97,92 @@ def convert_colour_to_rgba(colour, hsv=False):
         return to_rgba(colour)
 
 
-def interpolate_colours(col_limits, N, hsv=False):
+def interpolate_colours(
+    col_limits: npt.ArrayLike,
+    N: int,
+    hsv: bool = False,
+    wavelengths: npt.ArrayLike | None = None,
+    spacing: str = "linear",
+):
     """
     _summary_.
 
     Parameters
     ----------
-    col_limits : _type_
+    col_limits : npt.ArrayLike
         _description_.
-    N : _type_
+    N : int
         _description_.
     hsv : bool, optional
         _description_, by default False.
+    wavelengths : npt.ArrayLike | None, optional
+        _description_, by default None.
+    spacing : str, optional
+        _description_, by default "linear".
+
+    Returns
+    -------
+    _type_
+        _description_.
+
+    Raises
+    ------
+    Exception
+        _description_.
+    """
+
+    col_min = convert_colour_to_rgba(colour=col_limits[0], hsv=hsv)
+    col_max = convert_colour_to_rgba(colour=col_limits[1], hsv=hsv)
+
+    if N == 2:
+        return np.row_stack([col_min, col_max])
+
+    min_hsv = rgb_to_hsv(col_min[:-1])
+    max_hsv = rgb_to_hsv(col_max[:-1])
+
+    # hsv_diff
+    if wavelengths is None:
+        intervals = np.linspace(0, 1, N)
+    else:
+        wavelengths = np.asarray(wavelengths)
+        assert (
+            wavelengths.shape[0] == N
+        ), "A central wavelength must be provided for each filter."
+        intervals = (wavelengths - wavelengths[0]) / (wavelengths[-1] - wavelengths[0])
+    match spacing:
+        case "linear":
+            stretch = LinearStretch()
+        case "sqrt":
+            stretch = SqrtStretch()
+        case "log":
+            stretch = LogStretch()
+        case BaseStretch():
+            stretch = spacing
+        case _:
+            raise Exception("`spacing` is not a valid parameter.")
+    scaled_intervals = stretch(intervals)
+
+    interp_hsv = (np.outer(scaled_intervals, (max_hsv - min_hsv))) + min_hsv
+
+    interp_rgb = hsv_to_rgb(interp_hsv)
+    interp_rgba = np.column_stack(
+        (interp_rgb, np.linspace(col_min[-1], col_max[-1], N))
+    )
+    return interp_rgba
+
+
+def circle_coords(centre, radius, image_shape):
+    """
+    Adapted from `skimage.draw`.
+
+    Parameters
+    ----------
+    centre : _type_
+        _description_.
+    radius : _type_
+        _description_.
+    image_shape : _type_
+        _description_.
 
     Returns
     -------
@@ -107,15 +190,78 @@ def interpolate_colours(col_limits, N, hsv=False):
         _description_.
     """
 
-    col_min = convert_colour_to_rgba(colour=col_limits[0], hsv=hsv)
-    col_max = convert_colour_to_rgba(colour=col_limits[1], hsv=hsv)
+    centre = np.asarray(centre)
 
-    min_hsv = rgb_to_hsv(col_min[:-1])
-    max_hsv = rgb_to_hsv(col_max[:-1])
+    upper_left = np.ceil(centre - radius).astype(int)
+    lower_right = np.floor(centre + radius).astype(int)
 
-    interp_hsv = np.linspace(min_hsv, max_hsv, N)
-    interp_rgb = hsv_to_rgb(interp_hsv)
-    interp_rgba = np.column_stack(
-        (interp_rgb, np.linspace(col_min[-1], col_max[-1], N))
-    )
-    return interp_rgba
+    upper_left = np.maximum(upper_left, np.array([0, 0]))
+    lower_right = np.minimum(lower_right, np.array(image_shape[:2]) - 1)
+
+    shifted_centre = centre - upper_left
+    bounding_shape = lower_right - upper_left + 1
+
+    r_lim, c_lim = np.ogrid[0 : float(bounding_shape[0]), 0 : float(bounding_shape[1])]
+    r, c = (r_lim - shifted_centre[0]), (c_lim - shifted_centre[1])
+    rr, cc = np.nonzero((r**2 + c**2) < radius**2)
+    rr.flags.writeable = True
+    cc.flags.writeable = True
+    rr += upper_left[0]
+    cc += upper_left[1]
+    return rr, cc
+
+
+class RawAsinhTransform(BaseTransform):
+    """
+    A wrapper around AsinhStretch, where a is in non-normalised units.
+
+    Parameters
+    ----------
+    transform_interval : :class:`astropy.visualization.BaseInterval`
+        The interval transformation to apply to the image.
+    a : float, optional
+        The parameter a in the formula for AsinhStretch. This is given in raw
+        data units rather than normalised to the range 0-1, by default 0.01.
+    """
+
+    def __init__(self, transform_interval, a=0.01):
+        super().__init__()
+        self.a = a
+        self.transform_interval = transform_interval
+        # self._transf_a()
+        self.a_transf = a
+        self.transform_asinh = AsinhStretch(a=self.a_transf)
+
+    def __call__(self, values, clip=True):
+        normed_values = self.transform_interval(values, clip=clip)
+        vmin, vmax = self.transform_interval.get_limits(values)
+        self.a_transf = np.subtract(self.a, float(vmin))
+
+        if (vmax - vmin) != 0:
+            self.a_transf = np.true_divide(self.a_transf, vmax - vmin)
+
+        if clip:
+            np.clip(self.a_transf, 0.0, 1.0, out=self.a_transf)
+
+        self.transform_asinh = AsinhStretch(a=self.a_transf)
+
+        return self.transform_asinh(
+            self.transform_interval(values, clip=clip), clip=clip
+        )
+
+    @property
+    def inverse(self):
+        """
+        The inverse transformation.
+
+        This may not be accurate if the forward transformation has not been called
+        already, as `a_transf` can only be fully defined relative to the raw data.
+
+        Returns
+        -------
+        _type_
+            _description_.
+        """
+        return self.__class__(
+            self.transform_asinh.inverse, self.transform_interval.inverse
+        )
